6a70feface8e67f21a1640a3adb284a1818591fb
Naveen Kumar <naveenkumar.in@metamation.com>
2021-01-19 17:39:46 +0530

    Added references Microsoft.EntityFramework.Design, Npgsql, Npgsql.EntityFramework.PostgreSQL - migrate update with connection string and InitialCreate migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ArticleApp.API.Models;
+
 namespace ArticleApp.API.Data { 
     public class DataContext: DbContext {
         public DataContext(DbContextOptions<DataContext> options): base(options) {}
---
+++ Migrations/20210119115535_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ArticleApp.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    ArticleDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
